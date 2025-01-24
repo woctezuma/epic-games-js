a1e67c400be327b4c6b800eb6ef334f52c298642
Wok <woctezuma@users.noreply.github.com>
2025-01-24 20:31:54 +0100

    Data: add the JavaScript file DownloadPage.js

---
+++ data/DownloadPage.js
@@ -0,0 +1,19 @@
+"use strict";
+(self.__LOADABLE_LOADED_CHUNKS__ = self.__LOADABLE_LOADED_CHUNKS__ || []).push([
+  [590],
+  {
+    72639: (e, _, l) => {
+      l.r(_), l.d(_, { default: () => A });
+      var t = l(8788),
+        s = l(41791),
+        a = l(41855),
+        d = l(32150),
+        u = l(94916);
+      const A = () =>
+        (0, s.useSelector)(a.Y0)
+          ? t.createElement(u.A, { slug: "downloadv2" })
+          : t.createElement(d.default, { slug: "download" });
+    },
+  },
+]);
+//# sourceMappingURL=DownloadPage.egstore-site.81010e66b196547e1770.js.map
